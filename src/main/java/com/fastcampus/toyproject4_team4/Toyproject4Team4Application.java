5f054c4cd028def5480d2d808b55d8103cb97b3a
bill291104 <bill29123@gmail.com>
2025-07-14 17:35:48 +0900

    build(project): init project
    MySQL, thymeleaf, lombok, jpa, validation
    의존성 추가
    추가로 필요한 의존성은 진행하면서

---
+++ src/main/java/com/fastcampus/toyproject4_team4/Toyproject4Team4Application.java
@@ -0,0 +1,13 @@
+package com.fastcampus.toyproject4_team4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Toyproject4Team4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Toyproject4Team4Application.class, args);
+	}
+
+}
