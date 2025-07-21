5b37cc5abba264edbb643215e72ca7c5c7630fe3
너ìㅡãmonae <rrr6964@naver.com>
2025-07-21 15:44:02 +0900

    일단함

--- src/main/java/com/fastcampus/toyproject4_team4/Toyproject4Team4Application.java
+++ src/main/java/com/fastcampus/toyproject4_team4/Toyproject4Team4Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Toyproject4Team4Application {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(Toyproject4Team4Application.class, args);
 	}
 
