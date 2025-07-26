209e1d1b82fc3fea69b961b6a78523c62535d322
bill291104 <bill29123@gmail.com>
2025-07-26 19:20:46 +0900

    feat(vector_ss_service): 유사도 검색 엔드포인트 구현

--- python-fastapi-server/app/services/vector_ss_service.py
+++ python-fastapi-server/app/services/vector_ss_service.py
@@ -1,20 +1,26 @@
+from typing import List
+
 from ..schemas.vector_ss import SimilaritySearchRequest
 from langgraph.prebuilt import create_react_agent
 from ..llm.client import get_llm
+from .tools.vector_ss import main_domain_similarity_search, subdomain_filter
 
 async def similarity_search(request: SimilaritySearchRequest):
-    def get_weather(city: str) -> str:
-        """Get weather for a given city."""
-        return f"It's always sunny in {city}!"
-
-    prompt = "You are a helpful assistant"
-
-    model = get_llm(temperature=0.1)
-    agent = create_react_agent(model=model, tools=[get_weather], prompt=prompt)
-
-    result = await agent.ainvoke(
-        {"messages": [{"role": "user", "content": "what is the weather in sf"}]}
+    template = request.prompt_template
+    subdomains = request.target_domain.get_subs
+    prompt =  template.format(
+        main_domain=request.target_domain.value,
+        subdomains=subdomains,
+        keywords=request.target_keywords
     )
 
-    print(result.values())
-    return [1,2,3]
+    model = get_llm(temperature=0.1)
+    tools = [main_domain_similarity_search, subdomain_filter]
+    agent = create_react_agent(model=model, tools=tools, prompt=prompt, response_format=List[int], debug=True)
+
+    message = f"""
+    main_domain 에 해당하는 Collection 이름: {request.target_domain.value}
+    main_domain 에 종속된 subdomain 의 Collection 이름들: {request.target_domain.get_subs}
+    분류 후 유사도 검색에 사용될 target_keywords: {request.target_keywords}
+    """
+    return await agent.ainvoke({"messages": [("user", message)]})
