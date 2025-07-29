b267aecdda1f2fc6e5cf41325e2f35d4a8c75b64
lxxjxxyxxx <dlwnduf7884@naver.com>
2025-07-29 21:46:52 +0900

    feat(controller): controller 껍데기

---
+++ src/main/java/com/fastcampus/toyproject4_team4/controller/ManagerController.java
@@ -0,0 +1,11 @@
+package com.fastcampus.toyproject4_team4.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/manager")
+@RequiredArgsConstructor
+public class ManagerController {
+}
